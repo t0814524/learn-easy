6ca8f297ed3af5df09c6e6b637f3a7561f7b7487
Emanuel Soellinger <a01526271@unet.univie.ac.at>
2024-05-26 22:59:00 +0200

    adding dummy scoreboard v1

---
+++ components/Scoreboard.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
+
+export default class Scoreboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Rank', 'user', 'progress'],
+      tableData: [
+        ['#1', 'you', '47%'],
+        ['#2', 'friend#1', '32%'],
+        ['#3', 'friend#2', '31%'],
+        ['#4', 'friend#3', '2%']
+      ]
+    }
+  }
+
+  _alertIndex(index) {
+    Alert.alert(`This is row ${index + 1}`);
+  }
+
+  render() {
+    const state = this.state;
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => this._alertIndex(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>button</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold', paddingBottom: 10}}>Scoreboard:</Text>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper key={index} style={styles.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 3 ? element(cellData, index) : cellData} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#232B5D' },
+  head: { height: 40, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFFFFF' },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' }
+});
--- components/views/Statistics.tsx
+++ components/views/Statistics.tsx
@@ -1,7 +1,9 @@
 import { Text, View } from "react-native";
 import { FunctionComponent} from 'react';
 import { PieChart } from "react-native-gifted-charts";
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import { SettingsParams } from "../Layout";
+import Scoreboard from "../Scoreboard";
 
 class StatCategory{
     label: string = "";
@@ -29,224 +31,120 @@ export const StatisticsView: FunctionComponent<SettingsParams> = ({ mediums, car
     let todo = new StatCategory("todo", 3, "#FFA5BA", "#FF7F97");
 
     const pieData = [
-
         {
-      
           value: learned.value,
-      
           color: learned.color,
-      
           gradientCenterColor: learned.color2,
-      
           focused: true,
-      
         },
-      
         {value: learning.value, color: learning.color, gradientCenterColor: learning.color2},
-        
         {value: relearn.value, color: relearn.color, gradientCenterColor: relearn.color2},
-      
         {value: todo.value, color: todo.color, gradientCenterColor: todo.color2},
-      
     ];
 
     function renderDot(color: string){
-
         return (
-      
           <View
-      
             style={{
-      
               height: 10,
-      
               width: 10,
-      
               borderRadius: 5,
-      
               backgroundColor: color,
-      
               marginRight: 10,
-      
             }}
-      
           />
-      
         );
-      
     };
 
     function renderLegendComponent(){
-
         return (
-      
           <>
-      
             <View
-      
               style={{
-      
                 flexDirection: 'row',
-      
                 justifyContent: 'center',
-      
                 marginBottom: 10,
-      
               }}>
-      
               <View
-      
                 style={{
-      
                   flexDirection: 'row',
-      
                   alignItems: 'center',
-      
                   width: 120,
-      
                   marginRight: 20,
-      
                 }}>
-      
                 {renderDot(learned.color)}
-      
                 <Text style={{color: 'white'}}>{learned.label}: {learned.value}%</Text>
-      
               </View>
-      
               <View
-      
                 style={{flexDirection: 'row', alignItems: 'center', width: 120}}>
-      
                 {renderDot(relearn.color)}
-      
                 <Text style={{color: 'white'}}>{relearn.label}: {relearn.value}%</Text>
-      
               </View>
-      
             </View>
-      
             <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-      
               <View
-      
                 style={{
-      
                   flexDirection: 'row',
-      
                   alignItems: 'center',
-      
                   width: 120,
-      
                   marginRight: 20,
-      
                 }}>
-      
                 {renderDot(learning.color)}
-      
                 <Text style={{color: 'white'}}>{learning.label}: {learning.value}%</Text>
-      
               </View>
-      
               <View
-      
                 style={{flexDirection: 'row', alignItems: 'center', width: 120}}>
-      
                 {renderDot(todo.color)}
-      
                 <Text style={{color: 'white'}}>{todo.label}: {todo.value}%</Text>
-      
               </View>
-      
             </View>
-      
           </>
-      
         );
-      
     };
     
     return (
         <View
-      
           style={{
-      
-            paddingVertical: 100,
-      
+            paddingVertical: 20,
             backgroundColor: '#34448B',
-      
             flex: 1,
-      
           }}>
-      
           <View
-      
             style={{
-      
               margin: 20,
-      
               padding: 16,
-      
               borderRadius: 20,
-      
               backgroundColor: '#232B5D',
-      
             }}>
-      
             <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
-      
               {title}
-      
             </Text>
-      
             <View style={{padding: 20, alignItems: 'center'}}>
-      
               <PieChart
-      
                 data={pieData}
-      
                 donut
-      
                 showGradient
-      
                 sectionAutoFocus
-      
                 radius={90}
-      
                 innerRadius={60}
-      
                 innerCircleColor={'#232B5D'}
-      
                 centerLabelComponent={() => {
-      
                   return (
-      
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-      
                       <Text
-      
                         style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}>
-      
                         {learned.value}%
-      
                       </Text>
-      
                       <Text style={{fontSize: 14, color: 'white'}}>{learned.label}</Text>
-      
                     </View>
-      
                   );
-      
                 }}
-      
               />
-      
             </View>
-      
             {renderLegendComponent()}
-      
           </View>
-      
+          <Scoreboard />
         </View>
     );
+
 };
